44789d0aea6e3ca2adb12bfaf6100a557c15d065
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-03 14:29:09 +0600

    first commit

---
+++ fast_clas.js
@@ -0,0 +1,60 @@
+// At this stage, the data type and variables of JavaScript are discussed in detail below.
+
+// In JavaScript there are 5 different data types that can contain values:
+
+// string
+// number
+// boolean
+// object
+// function
+
+// ================= String ===============
+
+
+
+var Name = "MH RONY";
+
+console.log(Name);
+console.log(typeof Name);
+
+//  Print =>  MH RONY   ==> Data Type =  string;
+
+// ================= Number =====================
+
+var age = 20;
+
+console.log(age);
+console.log(typeof age);
+
+//  Print =>  20   ==> Data Type =  number;
+
+
+// ======================= boolean ===================
+
+var attend = true;
+var absent = false;
+
+console.log(attend);
+console.log(typeof attend);
+
+//  Print =>  true   ==> Data Type =  boolean;
+
+console.log(absent);
+console.log(typeof absent);
+//  Print =>  false   ==> Data Type =  boolean;
+
+
+// ======================= object ===================
+
+var info = null;
+
+console.log(info);
+console.log(typeof info);
+
+//  Print =>  null   ==> Data Type =  object;
+
+
+
+//================= function ==================
+
+
